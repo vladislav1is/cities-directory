9e5146df7b8770c4ef338225758d30ed0843e029
Vladislav Lisin <vladislav1isin@mail.ru>
2022-08-06 21:32:03 +0300

    Feature download directory of cities in Russia

--- src/com/redfox/citiesdirectory/Main.java
+++ src/com/redfox/citiesdirectory/Main.java
@@ -1,8 +1,43 @@
 package com.redfox.citiesdirectory;
 
+import com.redfox.citiesdirectory.model.City;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        String fileName = "Задача ВС Java Сбер.csv";
+        String dataDirectory = getDataDirectory();
+        printExistedData(dataDirectory, fileName);
+    }
+
+    private static String getDataDirectory() {
+        return Paths.get("data")
+                .toAbsolutePath()
+                .toString();
+    }
+
+    private static void printExistedData(String dataDirectory, String fileName) {
+        try {
+            printData(dataDirectory,fileName);
+        } catch (FileNotFoundException exception) {
+            System.out.println("File '" + fileName + "' not found in '" + dataDirectory +"'");
+            exception.printStackTrace();
+        }
+    }
+
+    private static void printData(String dataDirectory, String fileName) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new File(dataDirectory, fileName))) {
+            while (scanner.hasNextLine()) {
+                String row = scanner.nextLine();
+                String[] data = row.split(";");
+                City city = new City(data[1], data[2], data[3], Integer.parseInt(data[4]), data.length == 6 ? data[5] : "");
+                System.out.println(city);
+            }
+        }
     }
 }
---
+++ src/com/redfox/citiesdirectory/model/City.java
@@ -0,0 +1,28 @@
+package com.redfox.citiesdirectory.model;
+
+public class City {
+    private final String name;
+    private final String region;
+    private final String district;
+    private final int population;
+    private final String foundation;
+
+    public City(String name, String region, String district, int population, String foundation) {
+        this.name = name;
+        this.region = region;
+        this.district = district;
+        this.population = population;
+        this.foundation = foundation;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                ", foundation='" + foundation + '\'' +
+                '}';
+    }
+}
