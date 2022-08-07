1235723c2ed25e648afcf7909ce6f6ef676fac6f
Vladislav Lisin <vladislav1isin@mail.ru>
2022-08-07 16:40:41 +0300

    Feature reverse sorting of the directory by name, case-insensitive or by region and name.

--- src/com/redfox/citiesdirectory/Main.java
+++ src/com/redfox/citiesdirectory/Main.java
@@ -5,39 +5,57 @@ import com.redfox.citiesdirectory.model.City;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
     public static void main(String[] args) {
         String fileName = "Задача ВС Java Сбер.csv";
-        String dataDirectory = getDataDirectory();
-        printExistedData(dataDirectory, fileName);
+        String directoryName = "data";
+        String dataPath = getDataPath(directoryName);
+
+        Comparator<City> comparatorByName = Comparator.comparing(City::getName, String::compareToIgnoreCase);
+        Comparator<City> comparatorByDistrictAndName = Comparator.comparing(City::getDistrict).thenComparing(City::getName);
+
+        printExistedData(dataPath, fileName, comparatorByDistrictAndName);
     }
 
-    private static String getDataDirectory() {
-        return Paths.get("data")
+    private static String getDataPath(String directoryName) {
+        return Paths.get(directoryName)
                 .toAbsolutePath()
                 .toString();
     }
 
-    private static void printExistedData(String dataDirectory, String fileName) {
+    private static void printExistedData(String dataPath, String fileName, Comparator<City> comparator) {
         try {
-            printData(dataDirectory,fileName);
+            List<City> cities = sortData(dataPath, fileName, comparator);
+            cities.forEach(System.out::println);
         } catch (FileNotFoundException exception) {
-            System.out.println("File '" + fileName + "' not found in '" + dataDirectory +"'");
-            exception.printStackTrace();
+            System.out.println("File '" + fileName + "' not found in '" + dataPath + "'");
         }
     }
 
-    private static void printData(String dataDirectory, String fileName) throws FileNotFoundException {
-        try (Scanner scanner = new Scanner(new File(dataDirectory, fileName))) {
+    private static List<City> sortData(String dataPath, String fileName, Comparator<City> comparator) throws FileNotFoundException {
+        return readData(dataPath, fileName).stream()
+                .sorted(comparator.reversed())
+                .collect(toList());
+    }
+
+    private static List<City> readData(String dataPath, String fileName) throws FileNotFoundException {
+        List<City> cities = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(dataPath, fileName))) {
             while (scanner.hasNextLine()) {
                 String row = scanner.nextLine();
                 String[] data = row.split(";");
                 City city = new City(data[1], data[2], data[3], Integer.parseInt(data[4]), data.length == 6 ? data[5] : "");
-                System.out.println(city);
+                cities.add(city);
             }
         }
+        return cities;
     }
 }
--- src/com/redfox/citiesdirectory/model/City.java
+++ src/com/redfox/citiesdirectory/model/City.java
@@ -1,11 +1,13 @@
 package com.redfox.citiesdirectory.model;
 
+import java.util.Objects;
+
 public class City {
-    private final String name;
-    private final String region;
-    private final String district;
-    private final int population;
-    private final String foundation;
+    private String name;
+    private String region;
+    private String district;
+    private int population;
+    private String foundation;
 
     public City(String name, String region, String district, int population, String foundation) {
         this.name = name;
@@ -13,6 +15,63 @@ public class City {
         this.district = district;
         this.population = population;
         this.foundation = foundation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getFoundation() {
+        return foundation;
+    }
+
+    public void setFoundation(String foundation) {
+        this.foundation = foundation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return population == city.population
+                && Objects.equals(name, city.name)
+                && Objects.equals(region, city.region)
+                && Objects.equals(district, city.district)
+                && Objects.equals(foundation, city.foundation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, region, district, population, foundation);
     }
 
     @Override
